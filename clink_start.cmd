fa5f226d03f45d265f15faed5fd787b2b4086820
Giovanni Bassi <giggio@giggio.net>
2022-02-09 13:39:10 -0300

    Add aliases

--- clink_start.cmd
+++ clink_start.cmd
@@ -1,2 +1,26 @@
 @echo off
-set STARSHIP_CONFIG=C:\p\poshfiles\starship.toml
+IF EXIST %~dp0..\poshfiles\starship.toml set STARSHIP_CONFIG=%~dp0..\poshfiles\starship.toml
+
+doskey st=git status $*
+doskey k=kubectl $*
+doskey add=if "$*" == "" ( git add -A :/ ) else ( git add $* )
+doskey st=git status $*
+doskey push=git push $*
+doskey pull=git pull $*
+doskey log=git log $*
+doskey ci=git commit $*
+doskey co=git checkout $*
+doskey dif=git diff $*
+doskey rs=git reset $*
+doskey rb=git rebase $*
+doskey fixup=git fixup $*
+doskey branch=git branch $*
+doskey tag=git tag $*
+doskey up=git up $*
+doskey sync=git sync $*
+doskey l=ls $*
+doskey ll=ls -la $*
+doskey ccat=pygmentize -g -O style=vs -f console16m $*
+where hub >nul 2> nul && if ERRORLEVEL 0 doskey git=hub $*
+doskey pushsync=pwsh -NoProfile -NoLogo -NonInteractive -Command "& { $_branch = $(git rev-parse --abbrev-ref HEAD); git push --set-upstream origin $_branch; }"
+doskey gitbash=%ProgramFiles%\Git\usr\bin\bash.exe $*
