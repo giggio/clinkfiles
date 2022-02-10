d4169f44bfbcfc46527227421132dae04a45b7b9
Giovanni Bassi <giggio@giggio.net>
2022-02-09 23:18:17 -0300

    Add fzf and update install-script

--- install-scripts.ps1
+++ install-scripts.ps1
@@ -1,7 +1,10 @@
-$scriptsDir = Join-Path $PSScriptRoot "clink-completions"
 $clink = "${env:ProgramFiles(x86)}\clink\clink_x64.exe"
 $scripts = $(. $clink installscripts --list)
-if ("$scripts" -notlike "*$scriptsDir*") {
-    Write-Output "Adding $scriptsDir to clink scripts..."
-    . $clink installscripts $scriptsDir
+function Add-Script([string] $scriptPath) {
+    if ("$scripts" -notlike "*$scriptPath*") {
+        Write-Output "Adding $scriptPath to clink scripts..."
+        . $clink installscripts $scriptPath
+    }
 }
+Add-Script (Join-Path $PSScriptRoot "clink-completions")
+Add-Script (Join-Path $PSScriptRoot "clink-fzf")
